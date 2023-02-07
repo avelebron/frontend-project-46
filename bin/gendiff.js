f8b5f9346d59b99ec28e643be2ee6f614117e9e7
Vlada Lebron <peinfrantic@gmail.com>
2023-02-07 20:05:34 +0300

    upd gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,11 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0');
+
+program
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
