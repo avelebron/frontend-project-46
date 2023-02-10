002c95d149fe919cb8ad9e5d3781b33725e90359
Vlada Lebron <peinfrantic@gmail.com>
2023-02-10 16:44:10 +0300

    upd gendiff.js package-lock.json package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import genDiff from '../src/index.js';
 import { Command } from 'commander';
 const program = new Command();
 
@@ -7,9 +8,10 @@ program
   .name('gendiff')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
-
-program
-  .option('-f, --format <type>', 'output format');
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
