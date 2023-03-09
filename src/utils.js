ffa956fbe963abe7155dc1e85de9312181092de5
Vlada Lebron <peinfrantic@gmail.com>
2023-03-09 19:23:50 +0300

    path fix

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,4 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
-export default (file) => fs.readFileSync(path.resolve(process.cwd(), file), 'utf-8');
+export default (file) => fs.readFileSync(path.resolve(process.cwd(), 'frontend-project-46/__fixtures__/', file), 'utf-8');
