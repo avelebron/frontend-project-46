142c4eba34fb3e7b2d207cf540216cb19f8a9c54
Vlada Lebron <peinfrantic@gmail.com>
2023-03-11 19:56:28 +0300

    upd gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,9 +9,9 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   });
 
 program.parse();
