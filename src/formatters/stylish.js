6b9dccafb23556c8f23ce221ba3d28adbff5dce0
Vlada Lebron <peinfrantic@gmail.com>
2023-03-17 21:07:11 +0300

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,38 @@
 import _ from 'lodash';
 
-const stringify = (data, depth, replacer) => {
-  if (!_.isObject(data)) {
-    return `${data}`;
+const stylish = (currentKey, currentValue, depth) => {
+  if (!_.isObject(currentValue)) {
+    return `${currentKey}: ${currentValue}`;
   }
+  const indentBefore = '  '.repeat(depth);
+  const bracketIndent = '  '.repeat(depth - 1);
+  const lines = Object.entries(currentValue)
+    .flatMap(([key, ent]) => `  ${stylish(key, ent, depth + 2)}`);
 
-  const indentForKey = replacer.repeat(depth + 1);
-  const indentForBracket = replacer.repeat(depth);
-  const lines = Object.entries(data)
-    .map(([key, value]) => `${indentForKey}${key}: ${stringify(value, depth + 1, replacer)}`);
-
-  return ['{', ...lines, `${indentForBracket}}`].join('\n');
+  return `${currentKey}: {\n${indentBefore}${lines.join(`\n${indentBefore}`)}\n${bracketIndent}}`;
 };
 
-const sign = {
-  added: '+',
-  deleted: '-',
-  unchanged: ' ',
+const stylishToString = (tree) => {
+  const iter = (objects, depth) => {
+    const indentBefore = '  '.repeat(depth);
+    const bracketIndent = '  '.repeat(depth - 1);
+    const stylishedObjects = objects.flatMap((object) => {
+      const { name, value, status } = object;
+      const line = stylish(name, value, depth + 2);
+      if (status === 'removed') {
+        return `- ${line}`;
+      } if (status === 'added') {
+        return `+ ${line}`;
+      } if (status === 'unchanged') {
+        return `  ${line}`;
+      } if (status === 'updated') {
+        return [`- ${stylish(name, object.oldValue, depth + 2)}`, `+ ${line}`];
+      }
+      return `  ${name}: ${iter(value, depth + 2)}`;
+    });
+    return `{\n${indentBefore}${stylishedObjects.join(`\n${indentBefore}`)}\n${bracketIndent}}`;
+  };
+  return iter(tree, 1);
 };
 
-const makeStylish = (diff, replacer = '    ') => {
-  const iter = (tree, depth) => tree.map((node) => {
-    const indent = replacer.repeat(depth);
-    const indentForSign = indent.slice(2);
-
-    const makeLine = (value, mark) => `${indentForSign}${mark} ${node.key}: ${stringify(value, depth, replacer)}`;
-
-    switch (node.state) {
-      case 'added':
-        return makeLine(node.value, sign.added);
-      case 'deleted':
-        return makeLine(node.value, sign.deleted);
-      case 'notChanged':
-        return makeLine(node.value, sign.unchanged);
-      case 'changed':
-        return [`${makeLine(node.value1, sign.deleted)}`,
-          `${makeLine(node.value2, sign.added)}`].join('\n');
-      case 'nested':
-        return `${indent}${node.key}: ${['{', ...iter(node.value, depth + 1), `${indent}}`].join('\n')}`;
-      default:
-        throw new Error(`Type: ${node.state} is undefined`);
-    }
-  });
-
-  const stylishDiff = iter(diff, 1);
-  return ['{', ...stylishDiff, '}'].join('\n');
-};
-
-export default makeStylish;
+export default stylishToString;
