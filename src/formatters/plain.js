572cd8b7c59806d7775e39ff2500686d64810f80
Vlada Lebron <peinfrantic@gmail.com>
2023-03-19 21:27:34 +0300

    plain.js fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,33 +1,33 @@
 import _ from 'lodash';
 
-const plainFormatter = (tree) => {
-  const iter = (objects, ancestry) => {
-    const stylishedObjects = objects.flatMap((object) => {
-      const { name, value, status } = object;
-
-      const valueType = (val) => {
-        if (_.isObject(val)) {
-          return '[complex value]';
-        } if (_.isString(val)) {
-          return `'${val}'`;
-        }
-        return val;
-      };
-      const newName = [...ancestry, name].join('');
-      if (status === 'removed') {
-        return `Property '${newName}' was removed`;
-      } if (status === 'added') {
-        return `Property '${newName}' was added with value: ${valueType(value)}`;
-      } if (status === 'updated') {
-        return `Property '${newName}' was updated. From ${valueType(object.oldValue)} to ${valueType(value)}`;
-      } if (status === 'nested') {
-        return iter(value, `${newName}.`);
-      }
-      return [];
-    });
-    return `${stylishedObjects.join('\n')}`;
-  };
-  return iter(tree, '');
+const convert = (value) => {
+  if (_.isPlainObject(value)) {
+    return '[complex value]';
+  }
+  return _.isString(value) ? `'${value}'` : value;
 };
 
-export default plainFormatter;
+export default (data) => {
+  const iter = (curentValue, ancestry) => {
+    const lines = Object
+      .entries(curentValue)
+      .flatMap(([key, val]) => {
+        const newKey = `${ancestry}.${key}`;
+        const tree = _.trimStart(newKey, '.');
+        switch (val.type) {
+          case 'added':
+            return `Property '${tree}' was added with value: ${convert(val.value)}`;
+          case 'deleted':
+            return `Property '${tree}' was removed`;
+          case 'changed':
+            return `Property '${tree}' was updated. From ${convert(val.value1)} to ${convert(val.value2)}`;
+          case 'nested':
+            return `${iter(val.children, newKey)}`;
+          default:
+            return [];
+        }
+      }).join('\n');
+    return lines;
+  };
+  return iter(data, '');
+};
